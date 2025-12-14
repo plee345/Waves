f958d2a4f7f4a309b337f9b26e1a762ce7870501
Parker Lee <parkertlee@gmail.com>
2025-12-14 15:47:43 -0500

    Changed vertices size and background color

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 
 #include <iostream>
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
@@ -29,16 +43,77 @@ int main()
         std::cout << "Failed to initalize GLAD" << std::endl;
         return -1;
     }
+
+    float vertices[] = {
+    1.0f, 0.6f, 0.0f,
+     1.0f, -1.0f, 0.0f,
+     -1.0f,-1.0f, 0.0f,
+    -1.0f, 0.6f, 0.0f
+    };
     
+    unsigned int indices[]
+    {
+        0, 1, 3,
+        1, 2, 3
+    };
 
-    glViewport(0,0,800,600);
+    //initialize vertex array object and bind it for later use in render loop
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
 
+    //initialize vertex buffer object
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    //bind vertex data to the vertex buffer object
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //Initialize Element Buffer Object
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    //Bind indices to element buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    //initialize and link vertexShaderSource to vertexShader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    //initialize and link fragmentShaderSource to fragmentShader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    //initialize shaderProgram and link both vertex and fragment shaders to the shaderProgram
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    //clear data
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    //set vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);  
+
+    //rendering loop
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
-        glClearColor(0.820f, 0.787f, 0.164f, 2.5f);
+        glClearColor(0.870f, 0.861f, 0.322f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
